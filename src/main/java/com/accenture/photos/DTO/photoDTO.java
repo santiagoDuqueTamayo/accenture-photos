191bf4c1fae9f4e9c3284b304b11a5208cccb1b1
santiagohdt <santiagohdt@gmail.com>
2022-08-12 22:21:22 -0500

    coreccion sobre PhotoDTO

--- src/main/java/com/accenture/photos/DTO/photoDTO.java
+++ src/main/java/com/accenture/photos/DTO/photoDTO.java
@@ -1,9 +1,22 @@
 package com.accenture.photos.DTO;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
 
 @Data
-public class photoDTO {
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+@JsonNaming(PropertyNamingStrategy.LowerCaseStrategy.class)
+@Builder
+public class PhotoDTO implements Serializable {
 
     private Long id;
     private Long albumId;
